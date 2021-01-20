efc1e9791595b02ce591b67bd47691dca6e0644b
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-01-20 21:05:46 +0000

    time to play

--- js/index.js
+++ js/index.js
@@ -26,9 +26,14 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+let cardsFlipped = 1;
+let card1 = {};
+let card2 = {};
+let canFlip = true;
 
 window.addEventListener('load', event => {
   let html = '';
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
@@ -43,7 +48,43 @@ window.addEventListener('load', event => {
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      if(! canFlip)
+        return;
+      
+      canFlip = false;
+
       console.log(`Card clicked: ${card}`);
+      if(cardsFlipped < 2) {
+        cardsFlipped++;
+        card.classList.toggle('turned');
+        card1 = card;
+        canFlip = true;
+      } else {
+        cardsFlipped = 1;
+        card2 = card;
+        let isPair = memoryGame.checkIfPair(card1.getAttribute('data-card-name'), card2.getAttribute('data-card-name'));
+        
+        document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
+        document.getElementById('pairs-guessed').innerHTML = memoryGame.pairsGuessed;
+
+        card2.classList.toggle('turned');
+        if(isPair) {
+          card1.classList.toggle('blocked');
+          card2.classList.toggle('blocked');
+          canFlip = true;
+          if(memoryGame.isFinished()) {
+            setTimeout(() => {
+              alert('GAME OVER');
+            }, 600);
+          }
+        } else {
+          setTimeout(() => {
+            card1.classList.toggle('turned');
+            card2.classList.toggle('turned');
+            canFlip = true;
+          }, 1000);
+        }
+      }
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,46 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    if(! this.cards) {
+      return undefined;
+    }
+
+    let m = this.cards.length;
+    let temp = {};
+    let i = this.cards.length;
+
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      temp = this.cards[m];
+      this.cards[m] = this.cards[i]; //put the random card to the last position
+      this.cards[i] = temp;
+    }
+  }
+  
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if(card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
+  
+  isFinished() {
+    if(this.pairsGuessed == this.cards.length / 2) {
+      return true;
+    }
+    return false;
+  }
 }
